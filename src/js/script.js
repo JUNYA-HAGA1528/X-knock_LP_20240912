abb1419017604898946d307afd6d35fc1527a8f7
Haga Junya <n0812034@yahoo.co.jp>
2024-09-17 20:53:44 +0900

    2024/9/17

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,20 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-
+    document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+        anchor.addEventListener('click', function(event) {
+            event.preventDefault();  // デフォルトのページ内リンク動作を無効化
+            const targetId = this.getAttribute('href'); // クリックされたリンクのhref属性を取得
+            const target = document.querySelector(targetId); // 取得したIDからターゲット要素を選択
+    
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop - 80,  // スクロール位置を50px分調整
+                    behavior: 'smooth'           // スムーズなスクロール動作
+                });
+            }
+        });
+    });
+    
+    
 });
